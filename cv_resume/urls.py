45668278cd0668c36ac1b7ca9039c7bbbba105de
Ricardo <ricardoaomunoz@gmail.com>
2022-07-18 05:33:23 -0500

    add main urls to principal urls

--- cv_resume/urls.py
+++ cv_resume/urls.py
@@ -26,7 +26,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include('main.urls', namespace='main'))
+    path('api/', include('main.urls', namespace='main')),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
