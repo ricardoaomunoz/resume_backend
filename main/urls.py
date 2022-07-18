d5659ece5ca53d069d73e91bf73f62092ed5edb1
Ricardo <ricardoaomunoz@gmail.com>
2022-07-18 05:32:56 -0500

    add jwt create, refresh and verify to main urls

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,25 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 app_name = "main"
 
 urlpatterns = [
-    # path('', views.IndexView.as_view(), name="home"),
-    # path('', views.as_view(), name=""),
-    # path('', views.as_view(), name=""),
-    # path('', views.as_view(), name=""),
-    # path('', views.as_view(), name=""),
-    # path('', views.as_view(), name=""),    
-]
+    path('jwt/create/', TokenObtainPairView.as_view(), name='jwt_create'),
+    path('jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('jwt/verify/', TokenVerifyView.as_view(), name='token_verify')
+]   
+
+
+# urlpatterns = [
+#     # path('', views.IndexView.as_view(), name="home"),
+#     # path('', views.as_view(), name=""),
+#     # path('', views.as_view(), name=""),
+#     # path('', views.as_view(), name=""),
+#     # path('', views.as_view(), name=""),
+#     # path('', views.as_view(), name=""),    
+# ]
